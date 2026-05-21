b107393ffa781e2a16d8749fc1574b992851478a
luizcarlossilva-LH-BR <luiz.carlossilva@shopee.com>
2026-05-21 16:50:43 -0300

    Set BSC logo as site icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,11 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Ranking 3PL Shopee",
-  description: "Portal de relatório individual por transportadora"
+  description: "Portal de relatório individual por transportadora",
+  icons: {
+    icon: "/icon.png",
+    apple: "/icon.png"
+  }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
